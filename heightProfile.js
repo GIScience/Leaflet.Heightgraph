93d587bd00b8c64c2d8a851e47f5661dc717147d
Timothy Ellersiek <NeilMcCauley@NeilMcCauley.local>
2016-08-24 10:51:50 +0200

    css changes etc

--- heightProfile.js
+++ heightProfile.js
@@ -1,59 +1,3 @@
-var legendData = [{
-    '-5': {
-        text: '16%+',
-        color: '#028306' //gruen
-    }
-}, {
-    '-4': {
-        text: '10-15%',
-        color: '#2AA12E'
-    }
-}, {
-    '-3': {
-        text: '7-9%',
-        color: '#53BF56'
-    }
-}, {
-    '-2': {
-        text: '4-6%',
-        color: '#7BDD7E'
-    }
-}, {
-    '-1': {
-        text: '1-3%',
-        color: '#A4FBA6'
-    }
-}, {
-    '0': {
-        text: '0%',
-        color: '#ffcc99'
-    }
-}, {
-    '1': {
-        text: '1-3%',
-        color: '#F29898 '
-    }
-}, {
-    '2': {
-        text: '4-6%',
-        color: '#E07575'
-    }
-}, {
-    '3': {
-        text: '7-9%',
-        color: '#CF5352'
-    }
-}, {
-    '4': {
-        text: '10-15%',
-        color: '#BE312F'
-    }
-}, {
-    '5': {
-        text: '16%+',
-        color: '#AD0F0C' //rot
-    }
-}];
 /**
  * Returns distance between each coordinate of Linestring
  * @param {FeatureCollection} a
@@ -66,11 +10,11 @@ var calcDistances = function(a) {
     distances.distance = [];
     distances.wpDistance = [];
     var featureLength = a.features.length;
-    var wpPos=[];
-    var wpPosDist=[];
-    var lengthCoords=[];
-    var wpDistance=[];
-    distances.coordsOfDist=[];
+    var wpPos = [];
+    var wpPosDist = [];
+    var lengthCoords = [];
+    var wpDistance = [];
+    distances.coordsOfDist = [];
     for (var i = 0; i < featureLength; i++) {
         var coordLength = a.features[i].geometry.coordinates.length;
         console.log('coordLength', coordLength)
@@ -80,55 +24,61 @@ var calcDistances = function(a) {
             if (j == 0 && i > 0) {
                 calc = last.distanceTo(g);
                 distances.distance.push(calc);
-                distances.coordsOfDist.push([last,g]);
+                distances.coordsOfDist.push([last, g]);
             }
             var h = new L.LatLng(a.features[i].geometry.coordinates[j + 1][0], a.features[i].geometry.coordinates[j + 1][1]);
             calc = g.distanceTo(h);
             distances.distance.push(calc);
-            distances.coordsOfDist.push([g,h]);
+            distances.coordsOfDist.push([g, h]);
             // save last
             if (j == coordLength - 2) {
                 last = h;
             }
-        }//if Waypoints available
-        if (typeof(a.properties.waypoint_coordinates)!=="undefined"){
-            for (var l=0; l<a.properties.waypoint_coordinates.length; l++){
-                for (var j = 0; j < coordLength - 1; j++){
+        } //if Waypoints available
+        if (typeof(a.properties.waypoint_coordinates) !== "undefined") {
+            for (var l = 0; l < a.properties.waypoint_coordinates.length; l++) {
+                for (var j = 0; j < coordLength - 1; j++) {
                     var wpCoord = a.properties.waypoint_coordinates[l];
                     var Coord = a.features[i].geometry.coordinates[j];
                     //find Position of WayPoint in Feature: comparison of Coords and WayPoints
-                    if (wpCoord[0]==Coord[0]&&wpCoord[1]==Coord[1]){
-                    wpPos.push([i,j]);
+                    if (wpCoord[0] == Coord[0] && wpCoord[1] == Coord[1]) {
+                        wpPos.push([i, j]);
                     }
                 }
             }
         }
         lengthCoords[i] = coordLength;
     }
-    var WayPointList=[];
+    var WayPointList = [];
     var position = 0;
     // find Position of Waypoints and its distances
-    for (var o=0; o<wpPos.length; o++){
-        if(wpPos[o][0]==0){
+    for (var o = 0; o < wpPos.length; o++) {
+        if (wpPos[o][0] == 0) {
             position += wpPos[o][1];
             wpPosDist.push(position);
-            var wpArr=distances.distance.slice(0,(position+1));
-            wpDistance= wpArr.reduce(function(pv, cv) { return pv + cv; }, 0);
+            var wpArr = distances.distance.slice(0, (position + 1));
+            wpDistance = wpArr.reduce(function(pv, cv) {
+                return pv + cv;
+            }, 0);
             distances.wpDistance.push(wpDistance);
             WayPointList.push([position, wpDistance]);
-        }else{
-            for(var m=0; m<wpPos[o][0];m++){
+        } else {
+            for (var m = 0; m < wpPos[o][0]; m++) {
                 position = lengthCoords[m] // sum coords in last features
             }
-            position += wpPos[o][1]; 
+            position += wpPos[o][1];
             wpPosDist.push(position);
-            var wpArr=distances.distance.slice(0,(position+1));
-            wpDistance= wpArr.reduce(function(pv, cv) { return pv + cv; }, 0);
+            var wpArr = distances.distance.slice(0, (position + 1));
+            wpDistance = wpArr.reduce(function(pv, cv) {
+                return pv + cv;
+            }, 0);
             distances.wpDistance.push(wpDistance);
             WayPointList.push([position, wpDistance]);
         }
     }
-    distances.totaldistance = distances.distance.reduce(function(pv, cv) { return pv + cv; }, 0);
+    distances.totaldistance = distances.distance.reduce(function(pv, cv) {
+        return pv + cv;
+    }, 0);
     console.log(distances);
     return distances;
 };
@@ -140,7 +90,7 @@ var calcDistances = function(a) {
 var calculateHeightSteep = function(a) {
     var height = [];
     var steep = [];
-    for (var i = 0; i < a.features.length; i++){ 
+    for (var i = 0; i < a.features.length; i++) {
         var coordNumber = a.features[i].geometry.coordinates.length;
         for (var j = 0; j < coordNumber; j++) {
             height.push(a.features[i].geometry.coordinates[j][2]);
@@ -158,18 +108,18 @@ var calculateHeightSteep = function(a) {
  * @param {Array} a: steepness values of all coords
  * @returns {Array} list with steepness color and text as array
  */
-var updateLegend = function(a){
-    var legendList=[];
+var updateLegend = function(a) {
+    var legendList = [];
     //remove duplicates
     var cleanList = a.filter(function(elem, index, self) {
         return index == self.indexOf(elem);
-    }); 
-    for (var i=0; i<cleanList.length; i++){
-        var j= cleanList[i]-5;
+    });
+    for (var i = 0; i < cleanList.length; i++) {
+        var j = cleanList[i] - 5;
         legendList.push(legendData[cleanList[i]][j]);
     }
     return legendList;
-}
+};
 /**
  * Returns list with four x and y coordinates for svg-path (Polygon) and steepness type
  * @param {Array} a: heightvalue
@@ -178,7 +128,7 @@ var updateLegend = function(a){
  */
 var updateBarData = function(a, steepness) {
     var list = [];
-    var wplist= [];
+    var wplist = [];
     var adddist = [0];
     var count = a.length;
     for (var i = 0; i < count; i++) {
@@ -200,11 +150,9 @@ var updateBarData = function(a, steepness) {
             steepness: steepness[i],
             LatLng: distances.coordsOfDist[i]
         });
-    } 
+    }
     return list;
 };
-// a: distances
-//b: heightvalue
 /**
  * Returns list with two x and y coordinates for svg-path (Line and circle)
  * @param {Array|Object} a: distances
@@ -212,22 +160,21 @@ var updateBarData = function(a, steepness) {
  * @returns {Number|Array|Object} list with coordinates as array
  */
 var updateWaypointData = function(a, b) {
-    var list=[];
-    var length= a.wpDistance.length;
-    for (var i=0; i< length; i++){
+    var list = [];
+    var length = a.wpDistance.length;
+    for (var i = 0; i < length; i++) {
         list.push({
-            wpCoords:[{
+            wpCoords: [{
                 x: a.wpDistance[i],
                 y: d3.min(b)
             }, {
                 x: a.wpDistance[i],
-                y: (d3.max(b)/2)
-
+                y: (d3.max(b) / 2)
             }]
         });
     }
     return list;
-}
+};
 /**
  * @param {Object} polygonData: (x,y-coords, steepness)
  * @param {Object} waypointData: (x,y-coords)
@@ -255,11 +202,47 @@ var createBarChart = function(polygonData, waypointData, options, heightvalue, d
     });
     var svgSec = d3.select(options.div).append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     svgSec.call(tip);
+    // scale data (polygon-path)
+    var polygon = d3.svg.line().x(function(d) {
+        return x(d.x);
+    }).y(function(d) {
+        return y(d.y);
+    });
+    // bar chart as path
+    svgSec.selectAll('hpath').data(polygonData).enter().append('path')
+        //.attr('leafletId', id)
+        .attr('d', function(d) {
+            return polygon(d.coords);
+        })
+        //.attr("data-legend",function(d) { return d.steepness})
+        .attr("fill-opacity", 0.6).attr('fill', function(d) {
+            return (d.steepness - 5 == -5 ? '#028306' : d.steepness - 5 == -4 ? '#2AA12E' : d.steepness - 5 == -3 ? '#53BF56' : d.steepness - 5 == -2 ? '#7BDD7E' : d.steepness - 5 == -1 ? '#A4FBA6' : d.steepness - 5 == 0 ? '#ffcc99' : d.steepness - 5 == 1 ? '#F29898 ' : d.steepness - 5 == -2 ? '#E07575' : d.steepness - 5 == 3 ? '#CF5352' : d.steepness - 5 == 4 ? '#BE312F' : d.steepness - 5 == 5 ? '#AD0F0C' : '#AD0F0C');
+        }).on('mouseover', handleMouseOver).on("mouseout", handleMouseOut).on("mousemove", mousemove);
+    // focus line
     var focus = svgSec.append("g").attr("class", "focus").style("display", "none");
-    focus.append("circle").attr("r", 3);
-    focus.append("text").attr("x", 5).attr("font-size", "12px").attr("font-family", "calibri").attr("dy", ".35em").style("background", "black");
+    focus.append("text").attr("x", 5).attr("font-size", "12px").attr("dy", ".35em").style("background", "black");
     var focusLineGroup = svgSec.append("g").attr("class", "focusLine");
-    var focusLine = focusLineGroup.append("line").attr("stroke-width", 2).attr("stroke", "black").attr("x1", 10).attr("x2", 10).attr("y1", y(d3.max(heightvalue))).attr("y2", y(d3.min(heightvalue))).style("display", "none");
+    var focusLine = focusLineGroup.append("line").attr("stroke-width", 1).attr("stroke", "black").attr("x1", 10).attr("x2", 10).attr("y1", y(d3.max(heightvalue))).attr("y2", y(d3.min(heightvalue))).style("display", "none");
+    // waypoints line
+    svgSec.selectAll('linePath').data(waypointData).enter().append('path').attr('class', 'wpLine').attr('d', function(d) {
+        return polygon(d.wpCoords);
+    }).attr('pointer-events', 'none').attr("stroke-width", 2).attr('stroke', 'black');
+    // waypoints circle
+    svgSec.selectAll('hcircle').data(waypointData).enter().append('circle').attr('cx', function(d) {
+            return x(d.wpCoords[1].x);
+        }).attr('cy', function(d) {
+            return y(d.wpCoords[1].y);
+        }).attr('pointer-events', 'none').attr('r', 8)
+        //.attr("data-legend",function(d) { return d.steepness})
+        .attr('fill', 'black');
+    svgSec.selectAll('wpText').data(waypointData).enter().append("text").attr("class", "wp-text").attr("dx", function(d) {
+        return x(d.wpCoords[1].x) - 3.5;
+    }).attr("dy", function(d) {
+        return y(d.wpCoords[1].y - 3);
+    }).text(function(d, i) {
+        return i + 1;
+    }).style('fill', 'white').style('font-size', '12px');
+    // axes and axes labels
     svgSec.append('g').attr("transform", "translate(0," + height + ")") // create a <g> element
         .attr('class', 'x axis') // specify classes
         .call(xAxis).append("text")
@@ -271,6 +254,7 @@ var createBarChart = function(polygonData, waypointData, options, heightvalue, d
         'fill': 'none',
         'stroke-width': '1'
     });
+    // legend
     var legendRectSize = 7;
     var legendSpacing = 7;
     var legend = svgSec.selectAll('.legend').data(dynamicLegend).enter().append('g').attr('class', 'legend').attr('transform', function(d, i) {
@@ -280,65 +264,12 @@ var createBarChart = function(polygonData, waypointData, options, heightvalue, d
         var vert = i * height - offset;
         return 'translate(' + horz + ',' + vert + ')';
     });
-    legend.append('rect').attr('width', legendRectSize).attr('height', legendRectSize).attr('x', 30).attr('y', 30).style('fill', function(d, i){
+    legend.append('rect').attr('width', legendRectSize).attr('height', legendRectSize).attr('x', 30).attr('y', 30).style('fill', function(d, i) {
         return (d.color);
     });
     legend.append('text').attr('x', 40).attr('y', 36).style('font-size', 10).style('font-family', 'calibri').text(function(d, i) {
         return d.text;
     });
-    //scale data (polygon-path)
-    var polygon = d3.svg.line().x(function(d) {
-        return x(d.x);
-    }).y(function(d) {
-        return y(d.y);
-    });
-
-    //barChart as path
-    svgSec.selectAll('hpath').data(polygonData).enter().append('path')
-        //.attr('leafletId', id)
-        .attr('d', function(d) {
-            return polygon(d.coords);
-        })
-        //.attr("data-legend",function(d) { return d.steepness})
-        .attr("fill-opacity", 0.6).attr('fill', function(d) {
-            return (d.steepness - 5 == -5 ? '#028306' : d.steepness - 5 == -4 ? '#2AA12E' : d.steepness - 5 == -3 ? '#53BF56' : d.steepness - 5 == -2 ? '#7BDD7E' : d.steepness - 5 == -1 ? '#A4FBA6' : d.steepness - 5 == 0 ? '#ffcc99' : d.steepness - 5 == 1 ? '#F29898 ' : d.steepness - 5 == -2 ? '#E07575' : d.steepness - 5 == 3 ? '#CF5352' : d.steepness - 5 == 4 ? '#BE312F' : d.steepness - 5 == 5 ? '#AD0F0C' : '#AD0F0C');
-        }).on('mouseover', handleMouseOver).on("mouseout", handleMouseOut).on("mousemove", mousemove);
-
-        //Waypoints-Line
-    svgSec.selectAll('linePath').data(waypointData).enter().append('path').attr('class','wpLine')
-        .attr('d', function(d) {
-            return polygon(d.wpCoords);
-        })
-        .attr('pointer-events', 'none')
-        .attr("stroke-width", 2).attr('stroke', 'black');
-    //Waypoints-Circle
-    svgSec.selectAll('hcircle').data(waypointData).enter().append('circle')
-        .attr('cx', function(d) {
-            return x(d.wpCoords[1].x);
-        })
-        .attr('cy', function(d) {
-            return y(d.wpCoords[1].y);
-        })
-        .attr('pointer-events', 'none')
-        .attr('r', 8)
-        //.attr("data-legend",function(d) { return d.steepness})
-        .attr('fill', 'black');
-
-    svgSec.selectAll('wpText').data(waypointData).enter().append("text")
-        .attr("class", "wp-text")
-        .attr("dx", function(d) {
-            return x(d.wpCoords[1].x)-3.5;
-        })
-        .attr("dy", function(d) {
-            return y(d.wpCoords[1].y+5);
-        })
-        .text(function(d, i) {
-            return i+1;
-        })
-        .style('color','white')
-        .style('font-size', '12px')
-        .style('font-family','calibri');
-            
     // Create Event Handlers for mouse
     function handleMouseOver(d, i) {
         // Use D3 to select element, change color and size
@@ -361,8 +292,9 @@ var createBarChart = function(polygonData, waypointData, options, heightvalue, d
         var d2 = d1 - x0 > x0 - d0 ? 0 : 1; // shortest distance between mouse and coords of polygon
         var y0 = d.coords[d2].y;
         var LatLngCoords = d.LatLng;
-        console.log(LatLngCoords)
-        focus.style("display", "initial").attr("transform", "translate(" + x(x0) + "," + y(d3.min(heightvalue)) + ")");
+        var segmentCenter = L.latLngBounds(LatLngCoords[0], LatLngCoords[1]).getCenter();
+        console.log(segmentCenter);
+        focus.style("display", "initial").attr("transform", "translate(" + x(x0) + "," + y(d3.min(heightvalue) + 10) + ")");
         focus.select("text").text(Math.round((x0 / 1000) * 100) / 100 + ' km'); //text in km
         focusLine.style("display", "initial").attr('x1', x(x0)).attr('y1', y(y0)).attr('x2', x(x0));
     }
